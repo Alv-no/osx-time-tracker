16779074e51e274af738871d5eb40ce1190d428a
jantb <jan@boee.org>
2020-03-24 19:55:35 +0100

    More options

--- main.go
+++ main.go
@@ -7,12 +7,31 @@ import (
 	"log"
 	"os/exec"
 	"runtime"
+	"strconv"
+	"strings"
 	"time"
 )
+
+const sevenAndHalfHour = 27000000000000
 
 var lastPos = 0
 var lastTime = time.Now()
 var auto = true
+var week = Week{}
+
+type Week struct {
+	Monday    int
+	Tuesday   int
+	Wednesday int
+	Thursday  int
+	Friday    int
+	Saturday  int
+	Sunday    int
+}
+
+var autoTimeTresh = -15 * time.Minute
+var subTimeTresh = true
+var endOfDayNotice = false
 
 type TimeStruct struct {
 	ClockIn  time.Time
@@ -24,16 +43,37 @@ var times []TimeStruct
 func tracker() {
 	for {
 		duration := hoursForToday()
+		checkEndOfDayAndDisplayMessage(duration)
+
 		menuet.App().SetMenuState(&menuet.MenuState{
 			Title: fmtDuration(duration),
 		})
 		time.Sleep(200 * time.Millisecond)
 		if auto {
 			if !active() {
+				if subTimeTresh && canClockOut() {
+					subAutoTresh()
+				}
 				clockOutNow()
 			} else {
 				clockInNow()
 			}
+		}
+	}
+}
+
+func checkEndOfDayAndDisplayMessage(duration time.Duration) {
+
+	if duration.Nanoseconds() > sevenAndHalfHour && !endOfDayNotice {
+		endOfDayNotice = true
+		alert := menuet.App().Alert(menuet.Alert{
+			MessageText:     "Worked 7.5 hours today",
+			InformativeText: "Time to register hours",
+			Buttons:         []string{"Ok", "Open AlvTime"},
+			Inputs:          nil,
+		})
+		if alert.Button == 1 {
+			openAlvTime()
 		}
 	}
 }
@@ -68,13 +108,21 @@ func clockOutNowClicked() {
 	clockOutNow()
 }
 func clockOutNow() {
-	if len(times) != 0 && times[len(times)-1].ClockOut.IsZero() {
+	if canClockOut() {
 		times[len(times)-1].ClockOut = time.Now()
 	}
 }
 
+func canClockOut() bool {
+	return len(times) != 0 && times[len(times)-1].ClockOut.IsZero()
+}
+
 func toggleAuto() {
 	auto = !auto
+}
+
+func toggleSubAutotresh() {
+	subTimeTresh = !subTimeTresh
 }
 
 func reset() {
@@ -106,8 +154,100 @@ func sub30() {
 	})
 }
 
+func subAutoTresh() {
+	times = append(times, TimeStruct{
+		ClockIn:  time.Now(),
+		ClockOut: time.Now().Add(autoTimeTresh * time.Minute),
+	})
+}
+
 func openAlvTime() {
 	openbrowser("https://alvtime-vue-pwa-prod.azurewebsites.net/")
+}
+
+func copyAlvTime() {
+	robotgo.KeyTap("a", "command")
+	robotgo.KeyTap("c", "command")
+	time.Sleep(100 * time.Millisecond)
+	field, err := robotgo.ReadAll()
+	if err != nil {
+		return
+	}
+	week = Week{
+		Monday:    parseDecimalStringHoursToMinutes(field),
+		Tuesday:   readNextField(),
+		Wednesday: readNextField(),
+		Thursday:  readNextField(),
+		Friday:    readNextField(),
+		Saturday:  readNextField(),
+		Sunday:    readNextField(),
+	}
+}
+
+func pasteExperis() {
+	robotgo.PasteStr(fmt.Sprintf("%d:%d", week.Monday/60, week.Monday%60))
+	time.Sleep(1000 * time.Second)
+	robotgo.KeyTap("tab")
+	time.Sleep(1000 * time.Second)
+	robotgo.PasteStr(fmt.Sprintf("%d:%d", week.Tuesday/60, week.Tuesday%60))
+	time.Sleep(1000 * time.Second)
+	robotgo.KeyTap("tab")
+	time.Sleep(1000 * time.Second)
+	robotgo.PasteStr(fmt.Sprintf("%d:%d", week.Wednesday/60, week.Wednesday%60))
+	time.Sleep(1000 * time.Second)
+	robotgo.KeyTap("tab")
+	time.Sleep(1000 * time.Second)
+	robotgo.PasteStr(fmt.Sprintf("%d:%d", week.Thursday/60, week.Thursday%60))
+	time.Sleep(1000 * time.Second)
+	robotgo.KeyTap("tab")
+	time.Sleep(1000 * time.Second)
+	robotgo.PasteStr(fmt.Sprintf("%d:%d", week.Friday/60, week.Friday%60))
+	time.Sleep(1000 * time.Second)
+	robotgo.KeyTap("tab")
+	time.Sleep(1000 * time.Second)
+	robotgo.PasteStr(fmt.Sprintf("%d:%d", week.Saturday/60, week.Saturday%60))
+	time.Sleep(1000 * time.Second)
+	robotgo.KeyTap("tab")
+	time.Sleep(1000 * time.Second)
+	robotgo.PasteStr(fmt.Sprintf("%d:%d", week.Sunday/60, week.Sunday%60))
+}
+
+func readNextField() int {
+	robotgo.KeyTap("tab")
+	robotgo.KeyTap("c", "command")
+	time.Sleep(100 * time.Millisecond)
+	field, err := robotgo.ReadAll()
+	if err != nil {
+		return 0
+	}
+
+	return parseDecimalStringHoursToMinutes(field)
+}
+
+func parseDecimalStringHoursToMinutes(field string) int {
+	contains := strings.Contains(field, ",")
+	if contains {
+		split := strings.Split(field, ",")
+		if len(split) == 2 {
+			hours, err := strconv.Atoi(split[0])
+			if err != nil {
+				return 0
+			}
+			minutes, err := strconv.Atoi(split[1])
+			if err != nil {
+				return 0
+			}
+
+			return minutes + (hours * 60)
+		} else {
+			return 0
+		}
+	}
+	hours, err := strconv.Atoi(field)
+	if err != nil {
+		return 0
+	}
+	return hours * 60
 }
 
 func openExperis() {
@@ -118,6 +258,14 @@ func fmtDuration(d time.Duration) string {
 	h := d / time.Hour
 	d -= h * time.Hour
 	m := d / time.Minute
+	duration := sevenAndHalfHour * time.Nanosecond
+
+	doneBy := time.Now().Add(duration).Add(-d)
+
+	if canClockOut() {
+		return fmt.Sprintf("􀐱 %02d:%02d %s", h, m, doneBy.Format("15:04"))
+	}
+
 	return fmt.Sprintf("%02d:%02d", h, m)
 }
 
@@ -129,12 +277,27 @@ func active() bool {
 		lastTime = time.Now()
 		return true
 	}
-	if time.Now().Add(-15 * time.Minute).Before(lastTime) {
+	if time.Now().Add(autoTimeTresh * time.Minute).Before(lastTime) {
 		return true
 	}
 	return false
 }
 
+func autoTres5() {
+	autoTimeTresh = -5 * time.Minute
+}
+func autoTres10() {
+	autoTimeTresh = -10 * time.Minute
+}
+func autoTres15() {
+	autoTimeTresh = -15 * time.Minute
+}
+func autoTres30() {
+	autoTimeTresh = -30 * time.Minute
+}
+func autoTres60() {
+	autoTimeTresh = -60 * time.Minute
+}
 func menuItems() []menuet.MenuItem {
 	items := []menuet.MenuItem{
 		{
@@ -156,6 +319,43 @@ func menuItems() []menuet.MenuItem {
 			Type: menuet.Separator,
 		},
 		{
+			Text: "Adjust AutoTime",
+			Children: func() []menuet.MenuItem {
+				return []menuet.MenuItem{
+					{
+						Text:    "5m",
+						Clicked: autoTres5,
+						State:   autoTimeTresh == -5*time.Minute,
+					},
+					{
+						Text:    "10m",
+						Clicked: autoTres10,
+						State:   autoTimeTresh == -10*time.Minute,
+					},
+					{
+						Text:    "15m",
+						Clicked: autoTres15,
+						State:   autoTimeTresh == -15*time.Minute,
+					},
+					{
+						Text:    "30m",
+						Clicked: autoTres30,
+						State:   autoTimeTresh == -30*time.Minute,
+					},
+					{
+						Text:    "1h",
+						Clicked: autoTres60,
+						State:   autoTimeTresh == -60*time.Minute,
+					},
+					{
+						Text:    "Substract time after inactive",
+						Clicked: toggleSubAutotresh,
+						State:   subTimeTresh,
+					},
+				}
+			},
+		},
+		{
 			Text: "Adjust time",
 			Children: func() []menuet.MenuItem {
 				return []menuet.MenuItem{
@@ -168,12 +368,12 @@ func menuItems() []menuet.MenuItem {
 						Clicked: add15,
 					},
 					{
-						Text:    "Remove 15",
-						Clicked: sub15,
-					},
-					{
 						Text:    "Add 30",
 						Clicked: add30,
+					},
+					{
+						Text:    "Remove 15",
+						Clicked: sub15,
 					},
 					{
 						Text:    "Remove 30",
@@ -221,7 +421,7 @@ func main() {
 	app := menuet.App()
 	app.Children = menuItems
 	app.Label = "AlvMenuTime"
-	app.AutoUpdate.Version = "v0.1"
+	app.AutoUpdate.Version = "v0.2"
 	app.AutoUpdate.Repo = "jantb/time"
 	app.RunApplication()
 }
